fe3fbdb6743406917300c80b4e287be656ef0672
Cesar Garcia <79774422+cesare-garcia@users.noreply.github.com>
2023-08-23 13:31:38 -0500

    add reverse string test and function

--- src/practice.js
+++ src/practice.js
@@ -1,20 +1,27 @@
 // function capitalize -> takes a string and returns it with the first character capitalized
 
-function capitalize(inputString) {
-    const stringArray = inputString.split("");
-    stringArray[0] = stringArray[0].toUpperCase();
-    let moddedString = "";
-    for (let i = 0; i < stringArray.length; i++) {
-        moddedString += stringArray[i];
-    }
-    console.log(moddedString);
-    return moddedString;
-}
-module.exports = capitalize;
+// function capitalize(inputString) {
+//     const stringArray = inputString.split("");
+//     stringArray[0] = stringArray[0].toUpperCase();
+//     let moddedString = "";
+//     for (let i = 0; i < stringArray.length; i++) {
+//         moddedString += stringArray[i];
+//     }
+//     return moddedString;
+// }
+// module.exports = capitalize;
 
 // reverseString -> takes a string and returns it reversed
 
-
+function reverseString(inputString) {
+    const stringArray = inputString.split("");
+    let moddedString = "";
+    for (let i = stringArray.length - 1; i >= 0; i--) {
+        moddedString += stringArray[i];
+    }
+    return moddedString;
+}
+module.exports = reverseString;
 
 // calculator
 
--- src/practice.test.js
+++ src/practice.test.js
@@ -1,5 +1,9 @@
-const capitalize = require("./practice");
+const reverseString = require("./practice");
 
-test("returns a string with the first letter capitalized", () => {
-    expect(capitalize("programmer")).toBe("Programmer");
+// test("returns a string with the first letter capitalized", () => {
+//     expect(capitalize("programmer")).toBe("Programmer");
+// });
+
+test("return the input string reversed", () => {
+    expect(reverseString("the car is so fast")).toBe("tsaf os si rac eht");
 });
