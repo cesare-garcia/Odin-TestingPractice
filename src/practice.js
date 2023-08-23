74689cd410def1c2921f1bfd7db627ae9421a3e6
Cesar Garcia <79774422+cesare-garcia@users.noreply.github.com>
2023-08-23 13:44:40 -0500

    add tests and functions for add, subtract, divide, and multiply functions of the calculator object

--- src/practice.js
+++ src/practice.js
@@ -13,19 +13,33 @@
 
 // reverseString -> takes a string and returns it reversed
 
-function reverseString(inputString) {
-    const stringArray = inputString.split("");
-    let moddedString = "";
-    for (let i = stringArray.length - 1; i >= 0; i--) {
-        moddedString += stringArray[i];
-    }
-    return moddedString;
-}
-module.exports = reverseString;
+// function reverseString(inputString) {
+//     const stringArray = inputString.split("");
+//     let moddedString = "";
+//     for (let i = stringArray.length - 1; i >= 0; i--) {
+//         moddedString += stringArray[i];
+//     }
+//     return moddedString;
+// }
+// module.exports = reverseString;
 
 // calculator
 
-
+let calculator = {
+    add: function(a, b) {
+        return a + b;
+    },
+    subtract: function(a, b) {
+        return a - b;
+    },
+    divide: function(a, b) {
+        return a / b;
+    },
+    multiply: function(a, b) {
+        return a * b;
+    },
+};
+module.exports = calculator;
 
 // caesar cipher
 
--- src/practice.test.js
+++ src/practice.test.js
@@ -1,9 +1,25 @@
-const reverseString = require("./practice");
+const calculator = require("./practice");
 
 // test("returns a string with the first letter capitalized", () => {
 //     expect(capitalize("programmer")).toBe("Programmer");
 // });
 
-test("return the input string reversed", () => {
-    expect(reverseString("the car is so fast")).toBe("tsaf os si rac eht");
+// test("return the input string reversed", () => {
+//     expect(reverseString("the car is so fast")).toBe("tsaf os si rac eht");
+// });
+
+test("takes two numbers and returns the sum", () => {
+    expect(calculator.add(20,1)).toBe(21);
+});
+
+test("takes two numbers and returns the difference", () => {
+    expect(calculator.subtract(8,4)).toBe(4);
+});
+
+test("takes two numbers and returns the quotient", () => {
+    expect(calculator.divide(100,4)).toBe(25);
+});
+
+test("takes two numbers and returns the product", () => {
+    expect(calculator.multiply(4,4)).toBe(16);
 });
