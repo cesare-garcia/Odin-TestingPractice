69e1fb3c32b92208c73aaf7d67a456c06ae0e4fc
Cesar Garcia <79774422+cesare-garcia@users.noreply.github.com>
2023-08-23 16:08:10 -0500

    create test and function for the caesar cipher

--- src/practice.js
+++ src/practice.js
@@ -25,23 +25,89 @@
 
 // calculator
 
-let calculator = {
-    add: function(a, b) {
-        return a + b;
-    },
-    subtract: function(a, b) {
-        return a - b;
-    },
-    divide: function(a, b) {
-        return a / b;
-    },
-    multiply: function(a, b) {
-        return a * b;
-    },
-};
-module.exports = calculator;
+// let calculator = {
+//     add: function(a, b) {
+//         return a + b;
+//     },
+//     subtract: function(a, b) {
+//         return a - b;
+//     },
+//     divide: function(a, b) {
+//         return a / b;
+//     },
+//     multiply: function(a, b) {
+//         return a * b;
+//     },
+// };
+// module.exports = calculator;
 
 // caesar cipher
 
+// function caesarCipher(inputString, key) {
+//     let stringArray = inputString.split("");
+//     let moddedString = "";
+
+//     for (let i = 0; i < stringArray.length; i++) {
+//         if (containsSpecialChar(stringArray[i])) {
+//             moddedString += stringArray[i];
+//         } else if (containsUppercase(stringArray[i])) {
+//             let shiftedString = shiftUppercase(stringArray[i], key);
+//             moddedString += shiftedString;
+//         } else if (containsLowercase(stringArray[i])) {
+//             let shiftedString = shiftLowercase(stringArray[i], key);
+//             moddedString += shiftedString;
+//         } else {
+//             moddedString += stringArray[i];
+//         }
+//     }
+
+//     return moddedString;
+// };
+// module.exports = caesarCipher;
+
+// function containsUppercase(string) {
+//     return /[A-Z]/.test(string);
+// }
+
+// function containsLowercase(string) {
+//     return /[a-z]/.test(string);
+// }
+
+// function containsSpecialChar(string) {
+//     return /[0-9-'/`~!#*$@_%+=.,^&(){}[\]|;:”<>?\\]/.test(string);
+// }
+
+// function shiftUppercase(arrayElement, key) {
+//     let ucAlphabetArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+//     let elementCounter = 0;
+//     while (arrayElement !== ucAlphabetArray[elementCounter]) {
+//         elementCounter++;
+//     }
+
+//     elementCounter += key;
+
+//     while (elementCounter >= 26) {
+//         elementCounter -= 26;
+//     }
+
+//     return ucAlphabetArray[elementCounter];
+// }
+
+// function shiftLowercase(arrayElement, key) {
+//     let lcAlphabetArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    
+//     let elementCounter = 0;
+//     while (arrayElement !== lcAlphabetArray[elementCounter]) {
+//         elementCounter++;
+//     }
+
+//     elementCounter += key;
+
+//     while (elementCounter >= 26) {
+//         elementCounter -= 26;
+//     }
+
+//     return lcAlphabetArray[elementCounter];
+// }
 
 // analyzeArray function
--- src/practice.test.js
+++ src/practice.test.js
@@ -1,4 +1,4 @@
-const calculator = require("./practice");
+const analyzeArray = require("./practice");
 
 // test("returns a string with the first letter capitalized", () => {
 //     expect(capitalize("programmer")).toBe("Programmer");
@@ -8,18 +8,22 @@ const calculator = require("./practice");
 //     expect(reverseString("the car is so fast")).toBe("tsaf os si rac eht");
 // });
 
-test("takes two numbers and returns the sum", () => {
-    expect(calculator.add(20,1)).toBe(21);
-});
+// test("takes two numbers and returns the sum", () => {
+//     expect(calculator.add(20,1)).toBe(21);
+// });
 
-test("takes two numbers and returns the difference", () => {
-    expect(calculator.subtract(8,4)).toBe(4);
-});
+// test("takes two numbers and returns the difference", () => {
+//     expect(calculator.subtract(8,4)).toBe(4);
+// });
 
-test("takes two numbers and returns the quotient", () => {
-    expect(calculator.divide(100,4)).toBe(25);
-});
+// test("takes two numbers and returns the quotient", () => {
+//     expect(calculator.divide(100,4)).toBe(25);
+// });
 
-test("takes two numbers and returns the product", () => {
-    expect(calculator.multiply(4,4)).toBe(16);
-});
+// test("takes two numbers and returns the product", () => {
+//     expect(calculator.multiply(4,4)).toBe(16);
+// });
+
+// test("returns a string that has been shifted according to the key provided for the caesar cipher", () => {
+//     expect(caeserCipher("The secret is crime.", 113)).toBe("Cqn bnlanc rb larvn.");
+// });
